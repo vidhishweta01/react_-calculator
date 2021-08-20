66ddbef71c5ff513cc020aaf77a586b3daff975f
vidhishweta01 <vidhishweta01@gmail.com>
2021-08-20 16:19:33 +0530

    added logic added operate and calculate

--- src/component/App/App.js
+++ src/component/App/App.js
@@ -1,15 +1,12 @@
 import React from 'react';
-import styles from './App.module.css';
 import ButtonPanel from '../buttonPanel';
 import Display from '../Display';
 
 function App() {
   return (
     <>
-      <div className={styles.App}>
-        <Display />
-        <ButtonPanel />
-      </div>
+      <Display />
+      <ButtonPanel />
     </>
   );
 }
